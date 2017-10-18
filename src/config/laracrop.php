f382d6d49b3697f167041d6f3223519deb9f15cf
Aris Haryanto <arisharyanto32@gmail.com>
2017-10-18 15:18:16 +0700

    update missing config file

--- src/config/laracrop.php
+++ src/config/laracrop.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-	'route_prefix' => "laracrop", # route prefix to ajax url
-	'upload_url' => "upload/image", # route url to ajax upload url route_prefix/upload_url
-	'path_upload' => "/public/filetmp/", # where you put file upload
+	'route_prefix' => "laracrop", 		# route prefix to ajax url
+	'upload_url' => "upload/image",		# route url to ajax upload url route_prefix/upload_url
+	'path_upload' => "public/filetmp", 	# where you put file upload
+	'image_url' => "filetmp", 			# image url when ajax callback showing image
 	
   	'aspectratio' => "1",
   	'minsize' => "[200, 200]",
