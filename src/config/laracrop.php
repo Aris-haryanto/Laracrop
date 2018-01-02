6a3c08e98b652ab5e8b3684b40dc81435935d47b
= <arisharyanto32@gmail.com>
2018-01-02 23:55:24 +0700

    miss coma delimiter in config

--- src/config/laracrop.php
+++ src/config/laracrop.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-	'route_prefix' => "laracrop",
-	'upload_url' => "upload/image",
-	'path_upload' => "public/filetmp",
-	'image_url' => "filetmp",
+    'route_prefix' => "laracrop",
+    'upload_url' => "upload/image",
+    'path_upload' => "public/filetmp",
+    'image_url' => "filetmp",
 
-  	'aspectratio' => "0",
-  	'minsize' => "[200, 200]",
-  	'maxsize' => "[500, 500]",
-  	'bgcolor' => "black",
-  	'bgopacity' => "0.6"
-    'boxwidth' => "600"
+    'aspectratio' => "0",
+    'minsize' => "[200, 200]",
+    'maxsize' => "[500, 500]",
+    'bgcolor' => "black",
+    'bgopacity' => "0.6",
+    'boxwidth' => "600",
     'boxheight' => "600"
 ];
